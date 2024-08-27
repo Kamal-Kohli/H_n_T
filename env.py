b09ff162fd02080264c4091976bc6ebf79701139
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-27 17:45:07 +0200

    added secret keys and ture debug to env.py file

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["SECRET_KEY"] = 'django-insecure-h2w@x9=s3z-zuhym3-!_puah34wig!6civa!8kr*6s%2vvi5#t'
+os.environ["DEVELOPMENT"] = "True"
--- main_news/settings.py
+++ main_news/settings.py
@@ -9,21 +9,23 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2w@x9=s3z-zuhym3-!_puah34wig!6civa!8kr*6s%2vvi5#t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
