296a763f14106ee8d2fd746bd8bdfdcc60fb3d91
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-31 18:06:01 +0200

    add base html to login, signup, and logout html files

--- main_news/settings.py
+++ main_news/settings.py
@@ -151,6 +151,9 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+#STATIC_ROOT 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
