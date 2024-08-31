7cbcc774d3233825d6a1cd103bdd87a79543303f
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-31 18:11:52 +0200

    added email user setting in settings.py

--- main_news/settings.py
+++ main_news/settings.py
@@ -145,6 +145,15 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.your-email-provider.com'
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'your-email@example.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
+DEFAULT_FROM_EMAIL = 'your_email@gmail.com'  
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
