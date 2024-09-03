91d50e412a88d63c5e8a82fcfe6676142a7ddfeb
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-03 22:17:31 +0200

    updated users login as login required for access the news app

--- main_news/settings.py
+++ main_news/settings.py
@@ -90,6 +90,11 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+# Authentication settings
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 SITE_ID = 1
 
 WSGI_APPLICATION = 'main_news.wsgi.application'
