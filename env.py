5acdf82c49b799bd450c9e2521204ef686b2f468
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-03 23:28:53 +0200

    hide navbar and footer

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
 os.environ["SECRET_KEY"] = 'django-insecure-h2w@x9=s3z-zuhym3-!_puah34wig!6civa!8kr*6s%2vvi5#t'
-os.environ["DEVELOPMENT"] = 'True'
+os.environ["DEVELOPMENT"] = 'False'
 os.environ["API_KEY"] = '2228671f48644d399e8cc37fb14015f6'
